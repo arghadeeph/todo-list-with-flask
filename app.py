6cddc6a2b3946476a8be4ae7f4baed3e4ba317c1
Arghadeep Halder <arghadeeph@gmail.com>
2025-05-19 09:37:10 +0530

    email functionality addes

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from db import conn
 from flask import Flask, render_template, request, redirect, jsonify, flash
 import os
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 cursor = conn.cursor()
+
+# Configuration for mail server (example using Gmail SMTP)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'arghadeeph@gmail.com'
+app.config['MAIL_PASSWORD'] = 'oetlfgpykaignzej'  # Use app password, not Gmail password
+app.config['MAIL_DEFAULT_SENDER'] = 'arghadeeph@gmail.com'
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -51,6 +62,13 @@ def contact():
         message = request.form['message']
         cursor.execute("INSERT INTO contact_us (name, email, subject, message) values (%s, %s, %s, %s)", (name, email, subject, message))
         conn.commit()
+        msg = Message(
+        subject=subject,
+        recipients=[email],  # Change this
+        body=message
+        )
+        mail.send(msg)
+        flash('Thank you! Your message has been sent successfully.', 'success')
         return redirect('/contact')
     return render_template('contact.html')
 
